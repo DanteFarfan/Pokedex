d9e86a5a724b25b97cc5331c4fb834b738e267de
DanteFarfan <129178199+DanteFarfan@users.noreply.github.com>
2024-01-12 13:51:23 -0300

    Nueva version

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -1,142 +1,229 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+
+void main() => runApp(MyApp());
 
 class Pokemon {
-  final int id;
+  final int number;
   final String name;
-  final String img;
   final List<String> type;
+  final String imageUrl;
 
   Pokemon({
-    required this.id,
+    required this.number,
     required this.name,
-    required this.img,
     required this.type,
+    required this.imageUrl,
+  });
+}
+
+class PokemonDetail {
+  final double height;
+  final double weight;
+  final List<String> weaknesses;
+
+  PokemonDetail({
+    required this.height,
+    required this.weight,
+    required this.weaknesses,
   });
 }
 
 class MyApp extends StatelessWidget {
-  final List<Pokemon> pokemonList = [
-    Pokemon(
-      id: 1,
-      name: "Bulbasaur",
-      img: "assets/1.jpg",
-      type: ["Grass", "Poison"],
-    ),
-    Pokemon(
-      id: 2,
-      name: "Ivysaur",
-      img: "assets/2.png",
-      type: ["Grass", "Poison"],
-    ),
-    Pokemon(
-      id: 3,
-      name: "Venusaur",
-      img: "assets/3.png",
-      type: ["Grass", "Poison"],
-    ),
-    Pokemon(
-      id: 4,
-      name: "Charmander",
-      img: "assets/4.png",
-      type: ["Fire"],
-    ),
-     Pokemon(
-      id: 5,
-      name: "Charmeleon",
-      img: "assets/5.png",
-      type: ["Fire"],
-    ),
-    Pokemon(
-      id: 6,
-      name: "Charizard",
-      img: "assets/6.png",
-      type: ["Fire", "Flying"],
-    ),
-    Pokemon(
-      id: 7,
-      name: "Squirtle",
-      img: "assets/7.png",
-      type: ["Water"],
-    ),
-    Pokemon(
-      id: 8,
-      name: "Wartortle",
-      img: "assets/8.png",
-      type: ["Water"],
-    ),
-    Pokemon(
-      id: 9,
-      name: "Blastoise",
-      img: "assets/9.png",
-      type: ["Water"],
-    ),
-    Pokemon(
-      id: 10,
-      name: "Caterpie",
-      img: "assets/10.png",
-      type: ["Bug"],
-    ),
-
-
-
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Pokedex'),
-        ),
-        body: PokemonListView(pokemonList: pokemonList),
+      title: 'Pokedex',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: PokemonListScreen(),
     );
   }
 }
 
-class PokemonListView extends StatelessWidget {
-  final List<Pokemon> pokemonList;
+class PokemonListScreen extends StatefulWidget {
+  @override
+  _PokemonListScreenState createState() => _PokemonListScreenState();
+}
 
-  PokemonListView({required this.pokemonList});
+class _PokemonListScreenState extends State<PokemonListScreen> {
+  late List<Pokemon> pokemonList;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPokemonList();
+  }
+
+  Future<void> fetchPokemonList() async {
+    final response = await http.get(Uri.parse(
+        'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'));
+    final jsonData = json.decode(response.body);
+    final List<Pokemon> pokemonList = [];
+
+    for (var pokemonData in jsonData['pokemon']) {
+      pokemonList.add(Pokemon(
+        number: int.parse(pokemonData['num']),
+        name: pokemonData['name'],
+        type: List<String>.from(pokemonData['type']),
+        imageUrl: pokemonData['img'],
+      ));
+    }
+
+    setState(() {
+      this.pokemonList = pokemonList;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: pokemonList.length,
-      itemBuilder: (context, index) {
-        return PokemonCard(pokemon: pokemonList[index]);
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pokedex'),
+      ),
+      body: pokemonList == null
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: pokemonList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PokemonDetailScreen(
+                          pokemon: pokemonList[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: ListTile(
+                      leading: CachedNetworkImage(
+                        imageUrl: pokemonList[index].imageUrl,
+                        placeholder: (context, url) => CircularProgressIndicator(),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                      title: Text(
+                        '#${pokemonList[index].number.toString().padLeft(3, '0')} ${pokemonList[index].name}',
+                      ),
+                      subtitle: Text('Type: ${pokemonList[index].type.join(', ')}'),
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
 
-class PokemonCard extends StatelessWidget {
+class PokemonDetailScreen extends StatefulWidget {
   final Pokemon pokemon;
 
-  PokemonCard({required this.pokemon});
+  PokemonDetailScreen({required this.pokemon});
+
+  @override
+  PokemonDetailScreenState createState() => PokemonDetailScreenState();
+}
+
+class PokemonDetailScreenState extends State<PokemonDetailScreen> {
+  late PokemonDetail pokemonDetail;
+
+  @override
+  void initState() {
+    super.initState();
+    pokemonDetail = PokemonDetail(
+      height: 0.0,
+      weight: 0.0,
+      weaknesses: [],
+    );
+    fetchPokemonDetail();
+  }
+
+  Future<void> fetchPokemonDetail() async {
+    final response = await http.get(Uri.parse(
+        'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'));
+    final jsonData = json.decode(response.body);
+
+    final List<dynamic> pokemonList = jsonData['pokemon'];
+
+    final pokemonData = pokemonList.firstWhere(
+      (pokemon) => pokemon['num'] == widget.pokemon.number.toString().padLeft(3, '0'),
+      orElse: () => null,
+    );
+
+    if (pokemonData != null) {
+      double parseHeight(String height) {
+        try {
+          return double.tryParse(height.replaceAll(' m', '')) ?? 0.0;
+        } catch (e) {
+          return 0.0;
+        }
+      }
+
+      double parseWeight(String weight) {
+        try {
+          return double.tryParse(weight.replaceAll(' kg', '')) ?? 0.0;
+        } catch (e) {
+          return 0.0;
+        }
+      }
+
+      setState(() {
+        pokemonDetail = PokemonDetail(
+          height: parseHeight(pokemonData['height'].toString()),
+          weight: parseWeight(pokemonData['weight'].toString()),
+          weaknesses: List<String>.from(pokemonData['weaknesses']),
+        );
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2.0,
-      margin: EdgeInsets.all(8.0),
-      child: ListTile(
-        leading: Image.network(
-          pokemon.img,
-          width: 50,
-          height: 50,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.pokemon.name),
+      ),
+      body: Card(
+        margin: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 200,
+              child: CachedNetworkImage(
+                imageUrl: widget.pokemon.imageUrl,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Height',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('${pokemonDetail.height} m'),
+            ),
+            ListTile(
+              title: Text(
+                'Weight',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('${pokemonDetail.weight} kg'),
+            ),
+            ListTile(
+              title: Text(
+                'Weaknesses',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('${pokemonDetail.weaknesses.join(', ')}'),
+            ),
+          ],
         ),
-        title: Text(
-          "${pokemon.id}. ${pokemon.name}",
-          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text("Type: ${pokemon.type.join(', ')}"),
-        onTap: () {
-        },
       ),
     );
   }
